476dc37198584f485578aa30fbe35cb255467e38
Björn Åhström <ahstrom_81@hotmail.com>
2019-05-24 06:51:04 +0200

    Fixed so that the textFields in viewController is editable

--- TheGym/ViewController.swift
+++ TheGym/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var logoTextField: UITextField!
+    @IBOutlet weak var adressTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var openTextLabel: UILabel!
+    @IBOutlet weak var closingTextLabel: UILabel!
+    @IBOutlet weak var openTimeTextField: UITextField!
+    @IBOutlet weak var closingTimeTextField: UITextField!
+    @IBOutlet weak var editItemButton: UIBarButtonItem!
+    @IBOutlet var buttons: [UIButton]!
+    
+    var edit: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        editingTextFields(edit: edit)
+        setBorderRadiusOnButtonsAndLabels()
     }
-
-
+    
+    func editingTextFields(edit: Bool) {
+        logoTextField.isEnabled = edit
+        adressTextField.isEnabled = edit
+        phoneNumberTextField.isEnabled = edit
+        mailTextField.isEnabled = edit
+        openTimeTextField.isEnabled = edit
+        closingTimeTextField.isEnabled = edit
+    }
+    
+    @IBAction func editItemButton(_ sender: UIBarButtonItem) {
+        self.setEditing(!self.isEditing, animated: true)
+        let newButton = UIBarButtonItem(barButtonSystemItem: (self.isEditing) ? .done : .edit, target: self, action: #selector(editItemButton(_:)))
+        self.navigationItem.setRightBarButton(newButton, animated: true)
+        editingTextFields(edit: (self.isEditing) ? true : false)
+    }
+    
+    func setBorderRadiusOnButtonsAndLabels() {
+        for button in buttons {
+            button.layer.borderWidth = 1
+            button.layer.cornerRadius = 15
+            
+        }
+    }
 }
 
